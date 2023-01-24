44da37f559ade9bdd6739049045e3eb361060496
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-01-24 19:49:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.43%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    int numTrees(int n) {           //catalan numbers solution
+   /* int numTrees(int n) {           //catalan numbers solution
                 long ans = 1;
         for(int i = 1; i < n; i++)  
             ans = ans*(n+i+1) / i;    // do note that numerator and denominator will always be divisible
         return ans / n;
+    }*/
+    int dp[21];
+    int numTrees(int n){//using dp
+        if(n<=1)return 1;
+        if(dp[n])return dp[n];
+        for(int i=1;i<=n;i++){
+            dp[n]+=numTrees(i-1)*numTrees(n-i);
+        }
+         return dp[n];
     }
 };
