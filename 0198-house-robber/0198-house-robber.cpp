c6f97e0e7ec562c8028c339bfe77ab8cf34ef57e
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-01-26 11:45:13 +0530

    Time: 3 ms (36.59%), Space: 7.7 MB (80.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int dp[101]{};
-    int rob(vector<int>& nums) {
+    /*int rob(vector<int>& nums) {
        //non-dp solution
         int ans=INT_MIN,prev=0,curr=0;
         for(int i=0;i<nums.size();i++){
@@ -10,5 +10,15 @@ public:
             curr=t;
         }
         return curr;
+    }*/
+    int rob(vector<int>& nums){//dp solution
+        int n=size(nums);
+        if(n==1)return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
 };
