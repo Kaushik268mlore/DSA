0b739dc151bd94bbcb5f60864471ca9bd746c813
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-07 08:07:54 +0530

    Time: 4 ms (26.56%), Space: 6.1 MB (66.81%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     int integerBreak(int n) {
+    /* int integerBreak(int n) { // math based solution,because we can maximise it with the number of 3s and 2s
         if(n==2)return 1;
         if(n==3)return 2;
         int two=0;
@@ -26,5 +26,22 @@ public:
         n>>=1;
     }
     return apb;
-} 
+} */
+    
+    int integerBreak(int n){ //linear DP solution aka caching 
+        vector<int>dp(100);//dp[i] stores the maximum value of (product of breaks of the integer i)
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        dp[3]=2;
+        for(int i=4;i<=n;i++){
+            int num=i;
+            for(int j=1;j<=num;j++){
+               int x=j;
+                int y=num-j;
+                dp[i]=max({x*y,dp[i],dp[x]*y,x*dp[y],dp[x]*dp[y]});
+            }
+        }
+       return dp[n]; 
+    }
 };
