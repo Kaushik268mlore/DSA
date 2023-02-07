c77b5ab9997b907d2209c02b9229ea7189c07877
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-07 08:24:57 +0530

    Time: 3 ms (92.60%), Space: 9.5 MB (99.83%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {// an easy implementation based problem
+        for(int i = 0; i < n; i++){
+            nums[i] += nums[n + i] * 10000;
+        }
+       // at this point nums = [x1y1, x2y2, x3y3, y1, y2, y3].
+        
+       // we fill in the array values from the end, so as not to overwrite the calculated numbers in previous step
+      // at the end of one iteration nums will be [x1y1, x2y2, x3y3, y1, x3, y3 ]
+        for(int i = n - 1; i >= 0; i--){
+            nums[2 * i + 1] = nums[i] / 10000;
+            nums[2 * i] = nums[i] % 10000;
+        }
+        
+        return nums;
+    }
+};
