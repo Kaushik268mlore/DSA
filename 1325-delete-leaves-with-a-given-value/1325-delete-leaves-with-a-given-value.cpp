243af97865a01116b99fe1d27da23d81bf7fd995
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-24 01:02:54 +0530

    Time: 24 ms (26.70%), Space: 21.8 MB (26.70%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(root==nullptr)return nullptr;
+        root->right= removeLeafNodes(root->right,target);
+        root->left= removeLeafNodes(root->left,target);
+        
+        if(root->left==nullptr&&root->right==nullptr){
+            if(root->val==target)return nullptr;
+        }
+        return root;
+    }
+};
