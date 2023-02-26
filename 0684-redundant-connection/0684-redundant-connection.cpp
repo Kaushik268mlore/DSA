dda5adcaece3df24fe4ca88e5315eb97ffd9afcf
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-26 23:57:25 +0530

    Time: 4 ms (93.16%), Space: 8.9 MB (60.54%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // a very good DSU problem https://www.youtube.com/watch?v=1e67mQ-yBgE&t=0s refer if doubtful.
+    int find(int x,vector<int>&arr){
+        if(arr[x]==x)return x;
+        return find(arr[x],arr);
+    }
+    void union_(int a,int b,vector<int>&arr){
+        a=find(a,arr);
+        b=find(b,arr);
+        arr[a]=b;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        int n=e.size();
+        vector<int>parent(n+1);
+        for(int i=1;i<=n;i++)parent[i]=i;
+        for(auto arr:e){
+            if(find(arr[0],parent)==find(arr[1],parent))return arr;
+            union_(arr[0],arr[1],parent);
+        }
+    return {};}
+};
