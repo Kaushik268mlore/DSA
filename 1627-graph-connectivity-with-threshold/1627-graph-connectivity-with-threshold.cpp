f313bbce2898ea8ca55f570651a1b0b22631f36b
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-27 00:48:07 +0530

    Time: 189 ms (82.16%), Space: 65.2 MB (82.16%) - LeetHub

--- 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.cpp
+++ 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    // a DSU implementation
     int find(int x,vector<int>&arr){
         if(arr[x]==x)return x;
-        return arr[x]=find(arr[x],arr);
+        return arr[x]=find(arr[x],arr);//this is path compression where because of the nature of the problem we return its immediate parent
     }
     void union_(int a,int b,vector<int>&arr){
         a=find(a,arr);
@@ -14,7 +15,7 @@ public:
         vector<int>parent(n+1);
         //if(t==0)return vector<bool>(queries.size(),true);
         for(int i=1;i<=n;i++)parent[i]=i;
-        for(int i=t+1;i<=n;i++){
+        for(int i=t+1;i<=n;i++){//this logic is lit! cuz we consider the divisors greater than threshold and connect roads b/w aka make union among their multiples which lie in the range[1,n]
             int f=1;
             while(f*i<=n){
                 union_(f*i,i,parent);
