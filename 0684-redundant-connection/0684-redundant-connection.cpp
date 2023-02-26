4dc159f366ca439cf2e3476fea0069c2034c09cf
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-02-27 00:09:45 +0530

    Time: 10 ms (53.34%), Space: 9.1 MB (38.49%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -5,18 +5,39 @@ public:
         if(arr[x]==x)return x;
         return find(arr[x],arr);
     }
-    void union_(int a,int b,vector<int>&arr){
+    void union_(int a,int b,vector<int>&arr,vector<int>&rank){
         a=find(a,arr);
         b=find(b,arr);
-        arr[a]=b;
+        //below lines are the optimisation
+        if(rank[a]>rank[b]){
+            arr[b]=a;
+            rank[a]+=rank[b];
+        }
+        else{
+            arr[a]=b;
+            rank[b]+=rank[a];
+        }
+        
     }
     vector<int> findRedundantConnection(vector<vector<int>>& e) {
+    //     int n=e.size();      //this solution is normal DSU 
+    //     vector<int>parent(n+1);
+    //     for(int i=1;i<=n;i++)parent[i]=i;//intitially everyone's their own parent.
+    //     for(auto arr:e){
+    //         if(find(arr[0],parent)==find(arr[1],parent))return arr;//the edge with the same parent is the redundant edge
+    //         union_(arr[0],arr[1],parent);
+    //     }
+    // return {};
+    // below we have a slight optimisation , we add another array called rank that stores data of each node's children
+        // the idea is to choose the parent with more number of children 
         int n=e.size();
-        vector<int>parent(n+1);
-        for(int i=1;i<=n;i++)parent[i]=i;//intitially everyone's their own parent.
+        vector<int>par(n+1);
+        vector<int>rank(n+1);
+        for(int i=1;i<=n;i++)par[i]=i,rank[i]=1;
         for(auto arr:e){
-            if(find(arr[0],parent)==find(arr[1],parent))return arr;//the edge with the same parent is the redundant edge
-            union_(arr[0],arr[1],parent);
+            if(find(arr[0],par)==find(arr[1],par))return arr;//the edge with the same parent is the redundant edge
+            union_(arr[0],arr[1],par,rank);
         }
+            
     return {};}
 };
