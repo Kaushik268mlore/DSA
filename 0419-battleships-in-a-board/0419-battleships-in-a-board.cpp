5cf0252f5f62e4e62c9f0134e28636d79d40e869
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-03-09 19:26:00 +0530

    Time: 4 ms (96.35%), Space: 8.2 MB (24.88%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>&board , int i,int j){
+        int n=board.size(),m=board[0].size();
+        board[i][j]='.';
+        int dx[4]={-1,1,0,0};
+        int dy[4]={0,0,-1,1};
+        for(int k=0;k<4;k++){
+            int X=i+dx[k];
+            int Y=j+dy[k];
+            if(X>=0&&Y>=0&&X<n&&Y<m&&board[X][Y]=='X'){
+                dfs(board,X,Y);
+            }
+        }
+        }
+    int countBattleships(vector<vector<char>>& board) {
+        int n=board.size(),m=board[0].size(),ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='X'){
+                    ans++;
+                    cout<<ans<<" "<<i<<" "<<j<<endl;
+                    dfs(board,i,j);
+                }
+            }
+        }
+    return ans;}
+};
