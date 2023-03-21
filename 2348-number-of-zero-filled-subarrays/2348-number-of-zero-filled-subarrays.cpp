6d68c5ab457f84ca4bc82f6ee5cadbacdf4b80c3
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-03-21 12:21:55 +0530

    Time: 168 ms (75.72%), Space: 107.5 MB (86.43%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    //here the no.of zeroes is the only thing that matters along with how many occur consecutively
-    long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans=0,cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)cnt++;
-            else cnt=0;
-            ans+=cnt;
+    //here the no.of zeroes is the only thing that matters along with how many occur consecutively, 
+    // long long zeroFilledSubarray(vector<int>& nums) {
+    //     long long ans=0,cnt=0;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[i]==0)cnt++;
+    //         else cnt=0;
+    //         ans+=cnt;
+    //     }
+    // return ans;}
+    //pretty much the same logic but done using 2 pointers
+    
+    long long zeroFilledSubarray(vector<int>& nums){
+        long long int s=0,e=0,ans=0;
+        for(s=0,e=0;e<nums.size();e++){
+            if(nums[e]!=0)s=e+1;
+            ans+=(e-s+1);
         }
     return ans;}
 };
