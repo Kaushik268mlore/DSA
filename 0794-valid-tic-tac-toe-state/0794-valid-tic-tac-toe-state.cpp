8efaa737a7c766dcae30421015f06b179324fcc8
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-04-02 01:40:00 +0530

    Time: 3 ms (55.85%), Space: 8.4 MB (87.77%) - LeetHub

--- 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.cpp
+++ 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.cpp
@@ -1,28 +1,5 @@
 class Solution {
-private:
-//     bool winning(vector<string> &board, char c) {
-//         for(int i = 0; i < 3; i++)
-//             if(board[i][0] == c && board[i][1] == c && board[i][2] == c) return true;
-//         for(int j = 0; j < 3; j++)
-//             if(board[0][j] == c && board[1][j] == c && board[2][j] == c) return true;
-//         if(board[0][0] == c && board[1][1] == c && board[2][2] == c) return true;
-//         if(board[2][0] == c && board[1][1] == c && board[0][2] == c) return true;
-//         return false;
-//     }
-//     int count(vector<string> &board, char c) {
-//         int cnt = 0;
-//         for(auto r : board)
-//             for(auto val : r)
-//                 cnt += (c == val);
-//         return cnt;
-//     }
-    
-// public:
-//      bool validTicTacToe(vector<string>& board) {
-//         int xs = count(board, 'X'), os = count(board, 'O');
-//         return xs == os ? !winning(board, 'X') : xs == os + 1 ? !winning(board, 'O') : false;
-//     }
-    public:
+public:
     bool validTicTacToe(vector<string>& board){
         int cntX=0,cntO=0;
         for( string s:board){
