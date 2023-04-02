1b0bd3e9bed7195ad831cd3d84dd47abab731f63
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-04-02 17:52:47 +0530

    Time: 14 ms (83.01%), Space: 10.5 MB (12.60%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int cnt=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]<0)cnt++;
-            }
+    // int countNegatives(vector<vector<int>>& grid) { //this is very traditional O(n2)
+    //     int cnt=0;
+    //     for(int i=0;i<grid.size();i++){
+    //         for(int j=0;j<grid[0].size();j++){
+    //             if(grid[i][j]<0)cnt++;
+    //         }
+    //     }
+    // return cnt;}
+    //now we'll use the property of sortedness
+    /*
+    4,3,2,-1
+    3,2,1,-1
+    1,1,-1,-2
+    -1,-1,-2,-3 we can traverse from top right to bottom left this would at max be O(m+n)
+    */
+    int countNegatives(vector<vector<int>>& grid){
+        int n=grid.size(),m=grid[0].size(),cnt=0;
+        int r=0,c=m-1;
+        while(r<n){
+            while(c>=0&&grid[r][c]<0){c--;cnt++;}
+            c=m-1;r++;
         }
     return cnt;}
 };
