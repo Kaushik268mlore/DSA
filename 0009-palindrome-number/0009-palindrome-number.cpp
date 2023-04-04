d354d9aef1f6b018a077546f8f48bbf8f8de510c
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-04-04 22:51:11 +0530

    Time: 18 ms (46.88%), Space: 6 MB (63.50%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     //O(n) solution
-    bool isPalindrome(int x) {
-        string s=to_string(x);
-        string r=s;
-        reverse(s.begin(),s.end());
-        return r==s;
-    }
+    // bool isPalindrome(int x) {
+    //     string s=to_string(x);
+    //     string r=s;
+    //     reverse(s.begin(),s.end());
+    //     return r==s;
+    // }
+    bool isPalindrome(int x) { //good way to check a number is a palindrome or not in O(logn) time.
+        if(x<0||(x%10==0&&x!=0))return false;
+        int reverse=0;
+        while(x>reverse){// this is the condition that checks whether u are halfway yet or not
+            reverse*=10;
+            reverse+=x%10;
+            x/=10;
+        }
+    return x==reverse||x==reverse/10;}
 };
