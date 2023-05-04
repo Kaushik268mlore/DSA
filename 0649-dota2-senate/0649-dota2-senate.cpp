57e5be83b3ffc5014386a0c8a5a8b8f094288597
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-04 19:07:20 +0530

    Time: 8 ms (72.14%), Space: 8 MB (37.02%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int>r,d;
+        int n=senate.size();
+        for(int i=0;i<n;i++){
+            char c=senate[i];
+            if(c=='R')r.push(i);
+            if(c=='D')d.push(i);
+        }
+        while(!r.empty()&&!d.empty()){
+            int rid=r.front(),did=d.front();
+            r.pop();d.pop();
+            if(rid<did){
+                r.push(rid+n);
+            }
+            else d.push(did+n);
+        }
+    return (r.size()>d.size())?"Radiant":"Dire";}
+};
