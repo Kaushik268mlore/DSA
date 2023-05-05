3e8604a6290b17216d0c5fff0eba5e13e2914bee
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-05 19:19:46 +0530

    Time: 57 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.ts
@@ -0,0 +1,10 @@
+function createHelloWorld() {
+	// return function(...args): string {
+        return (...args: any[])=>"Hello World";
+    // };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
