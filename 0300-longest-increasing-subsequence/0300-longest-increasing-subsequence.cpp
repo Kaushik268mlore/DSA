6a7e36ba14752370dcea6e71c0d412dea1000766
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-07 23:32:13 +0530

    Time: 3 ms (99.62%), Space: 10.4 MB (76.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,13 +3,26 @@ public:
     // here dp[i] = LIS[0..i] 
     //recurrence relation : if(a greater number is found ) if(nums[i]>nums[j]&&dp[i]<dp[j]+1)dp[i]=dp[j]+1
     // as they are asking for the longest increasing subsequence return the longest in dp array.
-   int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < i; ++j)
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
-        return *max_element(dp.begin(), dp.end());
+   // int lengthOfLIS(vector<int>& nums) {
+   //      int n = nums.size();
+   //      vector<int> dp(n, 1);
+   //      for (int i = 0; i < n; ++i)
+   //          for (int j = 0; j < i; ++j)
+   //              if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+   //                  dp[i] = dp[j] + 1;
+   //      return *max_element(dp.begin(), dp.end());
+   //  }
+    // hiepit's binary search solution...
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
     }
 };
