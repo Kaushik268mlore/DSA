9e688634fc827d5af1e8ee784614d423b792eac3
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-07 23:28:43 +0530

    Time: 316 ms (58.83%), Space: 10.6 MB (57.36%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // here dp[i] = LIS[0..i] 
+    //recurrence relation : if(a greater number is found ) if(nums[i]>nums[j]&&dp[i]<dp[j]+1)dp[i]=dp[j]+1
+    // as they are asking for the longest increasing subsequence return the longest in dp array.
+   int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        return *max_element(dp.begin(), dp.end());
+    }
+};
