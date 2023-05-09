15670257efc6cce8b3dbe9715c1e84857f70b66e
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-09 07:45:25 +0530

    Time: 9 ms (39.37%), Space: 8 MB (52.45%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    /*vector<int> countBits(int n) {//a very easy math implementation,but can also be done using DP.
-      vector<int>arr;
-      for(int i=0;i<=n;i++){
-          arr.push_back(__builtin_popcount(i));
-      }
-    return arr;}*/
-    vector<int> countBits(int n) {//and this is the dp solution ,and solving it using DP aint easy..
-        vector<int>dp(n+1,0);
-        int off=1;
-        for(int i=1;i<=n;i++){
-            if(off*2==i)off=i;
-            dp[i]=1+dp[i-off];
+    vector<int> countBits(int n) {
+        vector<int>arr(n+1);
+        for(int i=0;i<=n;i++){
+            arr[i]=__builtin_popcount(i);
         }
-    return dp;}
+    return arr;}
 };
