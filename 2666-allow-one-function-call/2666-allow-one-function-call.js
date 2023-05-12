c34c723e7f97e5d7b84e78ba938e8d0c39c53331
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-05-13 00:41:13 +0530

    Time: 53 ms (85.79%), Space: 41.9 MB (62.02%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let hascalled=false;
+    return function(...args){
+         if(hascalled)return undefined;
+        else {hascalled=true;return fn(...args);}
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
