9fa2f978bf8c004f3f19e283284ea23f4d7a790a
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-07-04 17:14:56 +0530

    Time: 3 ms (78.38%), Space: 7.1 MB (8.82%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -2,35 +2,6 @@ class Solution {
 public:
     bool buddyStrings(string A, string B) {
         int n=A.length(),m=B.length();
-        if(n!=m)return false;
-        // unordered_map<char,int>mp;
-        // for(int i=0;i<m;i++){
-        //     mp[s[i]]=i;
-        // }
-        // unordered_set<char>s1(s.begin(),s.end());
-        // unordered_set<char>s2(goal.begin(),goal.end());
-        // if(s==goal&&s1.size()==s2.size())return true;
-        // bool done=0;
-        // for(int i=0;i<n;i++){
-        //     if(s[i]!=goal[i]){
-        //         if(mp.find(goal[i])!=mp.end()){
-        //             swap(s[mp[goal[i]]],s[i]);
-        //             done=1;
-        //             break;
-        //         }
-        //     }
-        // }
-        // BF wont work 
-        // for(int i=0;i<n-1;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         swap(s[i],s[j]);
-        //         if(s==goal)return true;
-        //         else{
-        //             swap(s[i],s[j]);
-        //         }
-        //     }
-        // }
-        
         if (A.length() != B.length()) return false;
         if (A == B && set<char>(A.begin(), A.end()).size() < A.size()) return true;
         vector<int> dif;
