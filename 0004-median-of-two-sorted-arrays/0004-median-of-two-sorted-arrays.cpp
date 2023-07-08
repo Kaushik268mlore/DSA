dfb85ee0018ff40a3cc97f17aabbaeeba6027d11
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-07-08 11:46:55 +0530

    Time: 36 ms (83.61%), Space: 90 MB (28.72%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,111 +1,15 @@
 class Solution {
 public:
-    /*double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int m=nums1.size();
-    int n=nums2.size();
-    if(m>n){
-        return findMedianSortedArrays(nums2,nums1);
-    }
-        int l=0,r=m;
-        int total=m+n+1;
-        
-        while(l<=r){
-            int fir=l+(r-l)/2;
-        int sec=total/2-fir;
-        int l1=INT_MIN,l2=INT_MIN;
-        int r1=INT_MAX,r2=INT_MAX;
-            if(fir>0){
-                l1=nums1[fir-1];
-            }
-            if(sec>0){
-                l2=nums2[sec-1];
-            }
-            if(fir>=0 and sec<m){
-                r1=nums1[fir];
-            }
-            if(fir>=0 and sec<n){
-                r2=nums2[sec];
-            }
-            if( l1<=r2 and l2<=r1){
-                if((n+m)%2 == 0){
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                }
-                else {
-                    return max(l1,l2);
-                }
-            }
-            else if(l1>r2){
-                r=fir-1;
-            }
-            else {
-                l=fir+1;
-            }
-        }
-        return 0;
-    }
-    
-};*/
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        if(m > n)
-        {
-            return findMedianSortedArrays(nums2, nums1);
+        vector<double>res(nums1.size()+nums2.size(),0);
+        int n=nums1.size(),m=nums2.size();
+        int l=res.size();
+        int j=0,k=0;
+        for(int i=0;i<l;i++){
+            res[i]=k==m||(j<=n-1&&nums1[j]<nums2[k])?nums1[j++]:nums2[k++];
         }
-        
-        int l=0, r= m;
-        int total = m+n+1;
-        
-        while(l<=r)
-        {
-            int fir = l + (r-l)/2;
-            int sec = total/2 - fir;
-            
-            int l1=INT_MIN, l2=INT_MIN;
-            int r1=INT_MAX, r2=INT_MAX;
-            
-            if(fir > 0)
-            {
-                l1 = nums1[fir-1];
-            }
-            if(sec>0)
-            {
-                l2 = nums2[sec-1];
-            }
-            if((fir>=0) && (fir<m))
-            {
-                r1 = nums1[fir];
-            }
-            if((sec>=0) && (sec<n))
-            {
-                r2 = nums2[sec];
-            }
-            
-            if(l1<=r2 && l2<=r1)
-            {
-                if((n+m)%2 == 0)
-                {
-                    return (max(l1, l2)+min(r1, r2))/2.0;
-                    
-                }
-                else
-                {
-                    return max(l1, l2);
-                }
-            }
-            else if(l1> r2)
-            {
-                r = fir-1;
-            }
-            else
-            {
-                l = fir+1;
-            }
-        }
-        
-        return 0;
-        
+        // double ans=0;
+        if(l&1)return res[l/2];
+        return (res[l/2-1]+res[l/2])/2;
     }
 };
