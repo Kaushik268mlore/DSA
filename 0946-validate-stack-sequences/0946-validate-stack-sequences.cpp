0fdfea4087e6251e7931ef25185adad220b72417
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-07-27 05:05:34 +0530

    Time: 14 ms (27.92%), Space: 15.4 MB (49.92%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    // in the popped array there should exist no pivot or at most one pivot and all the elements must exist in the pushed array.
-    //else we can a stack
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        // int n=pushed.size();
-        // int j=0;
-        // stack<int>s;
-        // for(int i=0;i<n;i++){
-        //     s.push(pushed[i]);
-        //     while(s.top()==popped[j]&&s.size())s.pop(),j++;
-        // }
-        // return s.empty();
-        stack<int> stack;
-        int i = 0;
-        for (int x : pushed) {
-            stack.push(x);
-            while (stack.size() && stack.top() == popped[i]) {
-                stack.pop();
-                i++;
-            }
+    bool validateStackSequences(vector<int> &push, vector<int> &pop) {
+        stack<int> s;
+        for (auto i = 0, j = 0; i < push.size(); ++i) {
+            s.push(push[i]);
+            while (!s.empty() && s.top() == pop[j]) s.pop(), ++j;
         }
-        return stack.size() == 0;
-    }
+    return s.empty();
+}
 };
