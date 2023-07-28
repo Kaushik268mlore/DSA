a9394e4ca24dc5304107fe7361b91cc7c490fa3c
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-07-28 22:49:52 +0530

    Time: 3 ms (37.07%), Space: 5.9 MB (88.31%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n=s.length();
+        int l=0,r=n-1;
+        while(l<r){
+            if(!isalpha(s[l])){
+                l++;
+            }
+            else if(!isalpha(s[r])){
+                r--;
+            }
+            else swap(s[l++],s[r--]);
+        }
+    return s;
+    }
+};
