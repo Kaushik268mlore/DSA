fcbe8b762eb2a97f95667bc4a058793193912382
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-07-31 01:08:38 +0530

    Time: 247 ms (84.84%), Space: 7 MB (81.69%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,24 +1,3 @@
-// #define LL long long 
-// class Solution {
-// public:
-//     // vector<vector<LL>>dp(305,vector<LL>(305,-1));
-//     // vector<vector<LL>>dp(305,vector<LL>(305, -1));
-//     LL dp[305][305];
-//     LL mod=1e9+7;
-//     int f(LL n,LL x,LL i){
-//         if(n==0)return 1;
-//         if(n<0||pow(i,n)>n)return 0;
-//         if(dp[n][i]!=-1)return dp[n][i];
-//         LL yes=f(n-pow(i,x),x,i+1);
-//         LL no=f(n,x,i+1);
-//         LL res=yes+no;
-//         return dp[n][i]=res%mod;
-//     }
-//     int numberOfWays(int n, int x) {
-//         memset(dp,-1,sizeof(dp));
-//         return f(n,x,1);
-//     }
-// };
 class Solution {
 public:
     long long dp[305][305];
