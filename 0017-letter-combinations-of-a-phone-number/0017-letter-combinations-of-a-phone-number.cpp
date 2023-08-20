d8acd4c9fc2bcb417c2a78185bee7696b2648898
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-08-20 05:55:24 +0530

    Time: 3 ms (38.81%), Space: 6.8 MB (13.17%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -32,8 +32,56 @@ public:
 };*/
 class Solution {
 public:
+//     vector<string> letterCombinations(string digits) {
+//         map<char,vector<char>> m;
+//         m['2'] = {'a','b','c'};
+//         m['3'] = {'d','e','f'};
+//         m['4'] = {'g','h','i'};
+//         m['5'] = {'j','k','l'};
+//         m['6'] = {'m','n','o'};
+//         m['7'] = {'p','q','r','s'};
+//         m['8'] = {'t','u','v'};
+//         m['9'] = {'w','x','y','z'};
+        
+//         vector<string> ans;
+//         int len = digits.size();
+        
+//         for(int i = 0; i<len; i++){
+//             char c = digits[i];
+//             vector<char> chars = m[c];
+            
+//             vector<string> tmpAns;
+//             vector<string> tmp = ans;
+            
+//             for(auto el : chars) {
+//                 if(i==0){
+//                     string s = "";
+//                     s += el;
+//                     ans.push_back(s);
+//                 }else {
+//                     for(auto str : tmp) tmpAns.push_back(str+el);
+//                     ans = tmpAns;
+//                 }
+//             }
+            
+//         }
+//         return ans;
+//     }
+//     gotta try the recursion solution out again!
+    map<char,vector<char>> m;
+    vector<string>res;
+    void rec(string digits,int start,int end,string curr){
+        if(start==end){
+            res.push_back(curr);
+            // return;
+        }
+        char id=digits[start];
+        for(int i=0;i<m[id].size();i++){
+            rec(digits,start+1,end,curr+m[id][i]);
+        }
+    }
     vector<string> letterCombinations(string digits) {
-        map<char,vector<char>> m;
+        if(digits.size()==0)return res;
         m['2'] = {'a','b','c'};
         m['3'] = {'d','e','f'};
         m['4'] = {'g','h','i'};
@@ -42,29 +90,7 @@ public:
         m['7'] = {'p','q','r','s'};
         m['8'] = {'t','u','v'};
         m['9'] = {'w','x','y','z'};
-        
-        vector<string> ans;
-        int len = digits.size();
-        
-        for(int i = 0; i<len; i++){
-            char c = digits[i];
-            vector<char> chars = m[c];
-            
-            vector<string> tmpAns;
-            vector<string> tmp = ans;
-            
-            for(auto el : chars) {
-                if(i==0){
-                    string s = "";
-                    s += el;
-                    ans.push_back(s);
-                }else {
-                    for(auto str : tmp) tmpAns.push_back(str+el);
-                    ans = tmpAns;
-                }
-            }
-            
-        }
-        return ans;
+        rec(digits,0,digits.length(),"");
+        return res;
     }
 };
