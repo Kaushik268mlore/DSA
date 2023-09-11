d6ed93a2a661a34368b3b0785b611618612d4a0a
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2023-09-11 23:32:55 +0530

    Time: 234 ms (68.53%), Space: 10.5 MB (71.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,15 +14,14 @@ public:
    //  }
     // hiepit's binary search solution...
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> sub;
-        for (int x : nums) {
-            if (sub.empty() || sub[sub.size() - 1] < x) {
-                sub.push_back(x);
-            } else {
-                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
-                *it = x; // Replace that number with x
+      // int dp[2501];
+        int n=nums.size();
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]&&dp[i]<dp[j]+1)dp[i]=dp[j]+1;
             }
         }
-        return sub.size();
+        return *max_element(dp.begin(),dp.end());
     }
 };
