2c8a1bd492436f540a7205629ca1f49faf5d8b4d
Kaushik Kumar <72248238+Kaushik268mlore@users.noreply.github.com>
2024-03-24 20:01:57 +0530

    Time: 47 ms (45.57%), Space: 17 MB (63.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+            string=''
+            for i in s:
+                if i.isalpha()or i.isdigit():
+                    string+=i.lower()
+            return string==string[::-1]
